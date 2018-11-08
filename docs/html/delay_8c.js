f9e8dbe19f2193bfc8a9303fa0a17e61e8e056fe
Michael Stephens <sudolegit@gmail.com>
2018-11-07 23:37:49 -0600

    Updating documentation (ran update scripts and saving output).
    Note:  Tweaked Doxyfile settings to skip inclusion of build objects.

---
+++ docs/html/delay_8c.js
@@ -0,0 +1,7 @@
+var delay_8c =
+[
+    [ "Delay_Init", "delay_8c.html#a3cf20e8cef38684bbbfbae93f3993c3b", null ],
+    [ "Delay_Ms", "delay_8c.html#af9304367b6c7e298f502aa6e90facd33", null ],
+    [ "Delay_Us", "delay_8c.html#aa74dfcd8d1885d63466a489dd3816123", null ],
+    [ "mCoreTimerFreq", "delay_8c.html#a750cac91948c87af890b5fdc8a4e71e8", null ]
+];
---
+++ docs/html/files.js
@@ -0,0 +1,5 @@
+var files =
+[
+    [ "delay.c", "delay_8c.html", "delay_8c" ],
+    [ "delay.h", "delay_8h.html", "delay_8h" ]
+];
